4c344e10438f62d6b4ea20320ef95c10cc73ddb1
joxiah <jmnyarega@gmail.com>
2020-09-18 06:17:18 +0300

    browser questions

--- src/Questions/index.tsx
+++ src/Questions/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { MouseEvent, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import Utils from "../utils/js";
 import "./index.css";
 
 const Questions = () => {
@@ -7,39 +8,71 @@ const Questions = () => {
   const { question: index } = useParams();
   const currentQuestion = questions[index];
 
+  const [correct, setCorrect] = useState(Utils.choices(questions, index)[1]);
+  const [answers, setAnswer] = useState(Utils.choices(questions, index)[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isCorrect, setIsCorrect] = useState();
+  const [clickedIndex, setClickedIndex] = useState(-1);
+
   const question = (question: string) => {
     return { __html: question };
   };
+
+  const checkAnswer = (_: MouseEvent, answer: number) => {
+    setClickedIndex(answer);
+    setIsCorrect(answer === correct);
+  };
+
+  useEffect(() => {
+    if (currentIndex !== index) {
+      setCorrect(Utils.choices(questions, index)[1]);
+      setAnswer(Utils.choices(questions, index)[0]);
+      setClickedIndex(-1);
+    }
+    setCurrentIndex(index);
+    // eslint-disable-next-line
+  }, [currentIndex, index]);
 
   return (
     <main className="content">
       <div className="content-question__container">
         <h2
-          className="content-title title-small"
+          className="content-title title-small question-title"
           dangerouslySetInnerHTML={question(currentQuestion.question)}
         />
       </div>
-      <div className="content-btn__container">
-        <div className="cta">
-          {index >= 1 && (
-            <Link
-              to={{ pathname: `/question/${Number(index) - 1}` }}
-              className="btn btn-ghost"
-            >
-              previous
-            </Link>
-          )}
+
+      {answers.map((answer: any, i: number) => (
+        <div className="content-answers">
+          <button
+            className={`content-answer ${
+              isCorrect === true && i === clickedIndex
+                ? "correct"
+                : isCorrect === false && i === clickedIndex
+                ? "wrong"
+                : "no answer"
+            }`}
+            disabled={clickedIndex >= 0}
+            onClick={(e) => checkAnswer(e, i)}
+          >
+            <span dangerouslySetInnerHTML={question(answer)} />
+          </button>
         </div>
-        <div className="cta">
-          {index < questions.length - 1 && (
-            <Link
-              to={{ pathname: `/question/${Number(index) + 1}` }}
-              className="btn btn-ghost"
-            >
-              Next
-            </Link>
-          )}
-        </div>
+      ))}
+      <div className="cta">
+        {index < questions.length - 1 && clickedIndex >= 0 && (
+          <Link
+            to={{ pathname: `/question/${Number(index) + 1}` }}
+            className="btn btn-ghost"
+          >
+            Next
+          </Link>
+        )}
+        {Number(index) === questions.length - 1 && clickedIndex >= 0 && (
+          <Link to={{ pathname: `/results` }} className="btn btn-ghost">
+            Submit
+          </Link>
+        )}
       </div>
     </main>
   );
--- src/utils/js/index.ts
+++ src/utils/js/index.ts
@@ -7,6 +7,22 @@ class Utils {
     }));
     return [{ options: result, label }];
   };
+  choices = (questions: any, index: any) => {
+    const currentQuestion = questions[index];
+    const organise = new Set();
+    const correct = currentQuestion.correct_answer;
+    const allAnswers = currentQuestion.incorrect_answers.concat(correct);
+
+    for (let x = 0; x <= allAnswers.length * 30; x++) {
+      let random = Math.floor(Math.random() * Math.floor(allAnswers.length));
+      organise.add(random);
+    }
+
+    const organised = Array.from(organise);
+    //@ts-ignore
+    const shuffled = organised.map((index: number): any => allAnswers[index]);
+    return [shuffled, shuffled.indexOf(correct)];
+  };
 }
 
 export default new Utils();
